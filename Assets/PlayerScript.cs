5e0168b7ed2eff5f2936187b199aea39dfb10831
Rustin Wollin <rwollin96@gmail.com>
2014-10-17 19:17:39 -0400

    initial commit
    starting out with a movable player graphic stand-in

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed = 2.0f;
+
+	private Vector3 mousePos;
+	private Vector3 velocity = new Vector3(0f, 0f, 0f);
+
+	void Start() {
+
+	}
+	
+	void Update() {
+		mousePos = Input.mousePosition;
+		Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+		mousePos.x -= objectPos.x;
+		mousePos.y -= objectPos.y;
+
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+		velocity.x = Input.GetAxis("Horizontal") * speed;
+		velocity.y = Input.GetAxis("Vertical") * speed;
+	}
+
+	void FixedUpdate() {
+		transform.position += velocity;
+	}
+}
