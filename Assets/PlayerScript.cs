8b62b221a4008a3ffac359391dc0a9c940e95cd1
Rustin Wollin <rwollin96@gmail.com>
2014-10-17 20:41:04 -0400

    sword art, mechanics, animation

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,12 +6,14 @@ public class PlayerScript : MonoBehaviour {
 
 	private Vector3 mousePos;
 	private Vector3 velocity = new Vector3(0f, 0f, 0f);
+	private Animator animator;
 
 	void Start() {
-
+		animator = gameObject.GetComponent<Animator>();
 	}
 	
 	void Update() {
+		//rotation
 		mousePos = Input.mousePosition;
 		Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
 		mousePos.x -= objectPos.x;
@@ -20,8 +22,14 @@ public class PlayerScript : MonoBehaviour {
 		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
+		//movement
 		velocity.x = Input.GetAxis("Horizontal") * speed;
 		velocity.y = Input.GetAxis("Vertical") * speed;
+
+		//fire
+		if (Input.GetButtonDown("Fire1")) {
+			animator.SetTrigger("swingSword");
+		}
 	}
 
 	void FixedUpdate() {
